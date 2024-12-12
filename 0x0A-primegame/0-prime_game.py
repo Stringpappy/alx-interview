d00a6fcb193977382d7eecc57da31c36ff5c36c3
stringpappy <jejeodunayo70@gmail.com>
2024-12-12 21:47:27 +0100

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,54 +1,61 @@
 #!/usr/bin/python3
-"""Module defining isWinner function."""
+"""Module for Prime Game"""
 
 def isWinner(x, nums):
-    """Function to determine the winner of the prime game."""
-    maria_wins_count = 0
-    ben_wins_count = 0
+    """
+    Determines the winner of a set of prime number removal games.
 
-    for number in nums:
-        rounds_set = list(range(1, number + 1))
-        primes_set = primes_in_range(1, number)
+    Args:
+        x (int): The number of rounds.
+        nums (list of int): A list of integers where each integer n denotes
+        a set of consecutive integers starting from 1 up to and including n.
 
-        if not primes_set:
-            ben_wins_count += 1
-            continue
+    Returns:
+        str: The name of the player who won the most rounds (either "Ben"
+        or "Maria").
+        None: If the winner cannot be determined.
 
-        is_maria_turn = True
+    Raises:
+        None.
 
-        while True:
-            if not primes_set:
-                if is_maria_turn:
-                    ben_wins_count += 1
-                else:
-                    maria_wins_count += 1
-                break
-
-            smallest_prime = primes_set.pop(0)
-            rounds_set.remove(smallest_prime)
-
-            rounds_set = [value for value in rounds_set if value % smallest_prime != 0]
-
-            is_maria_turn = not is_maria_turn
-
-    if maria_wins_count > ben_wins_count:
-        return "Winner: Maria"
-
-    if maria_wins_count < ben_wins_count:
-        return "Winner: Ben"
-
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+    ben = 0
+    maria = 0
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        mark_non_primes(a, i)
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
     return None
 
-def is_prime(number):
-    """Returns True if the number is prime, else False."""
-    if number < 2:
-        return False
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
+def mark_non_primes(ls, x):
+    """
+    Marks multiples of a given number as non-prime in the list of potential primes.
 
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [value for value in range(start, end + 1) if is_prime(value)]
-    return primes
+    Args:
+        ls (list of int): An array representing potential prime numbers.
+        x (int): The number whose multiples are to be marked as non-prime.
+
+    Returns:
+        None.
+
+    Raises:
+        None.
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
